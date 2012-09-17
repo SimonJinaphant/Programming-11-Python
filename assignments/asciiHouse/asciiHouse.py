7f90540627661b8564d0bdbfb1beba0ce371e378
Simon Jinaphant <user@server>
2012-09-16 22:50:40 -0700

    Add first assignment

---
+++ assignments/asciiHouse/asciiHouse.py
@@ -0,0 +1,18 @@
+print("\t\t\t-------------------------");
+print("\t\t       /\t\t\t \\");
+print("\t\t      /\t\t\t\t  \\");
+print("\t\t     /\t\t\t\t   \\");
+print("\t\t    /\t\t\t\t    \\");
+print("\t\t   /\t\t\t\t     \\");
+print("\t\t  -------------------------------------");
+print("\t\t  |\t\t\t\t      |");
+print("\t\t  |\t\t\t\t      |");
+print("\t\t  |\t\t\t\t      |");
+print("\t\t  |\t----------\t     -------  |");
+print("\t\t  |\t|   |    |\t     |     |  |");
+print("\t\t  |\t|---|----|\t     |     |  |");
+print("\t\t  |\t|   |    |\t     |     |  |");
+print("\t\t  |\t----------\t     |     |  |");
+print("\t\t  -------------------------------------");
+
+print("\n\n\t\t\t\t - Simon Jinaphant");
