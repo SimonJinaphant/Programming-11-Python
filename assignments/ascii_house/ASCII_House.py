c9ea1d96d635a51b9ef22977543441ff098eae2a
Simon Jinaphant <symon.jin@gmail.com>
2012-09-21 21:35:09 -0700

    Updated ASCII-House assignment

---
+++ assignments/ascii_house/ASCII_House.py
@@ -0,0 +1,22 @@
+'''
+	ASCII House - Simple version
+'''
+
+print "\t\t\t-------------------------"
+print "\t\t       /\t\t\t \\"
+print "\t\t      /\t\t\t\t  \\"
+print "\t\t     /\t\t\t\t   \\"
+print "\t\t    /\t\t\t\t    \\"
+print "\t\t   /\t\t\t\t     \\"
+print "\t\t  -------------------------------------"
+print "\t\t  |\t\t\t\t      |"
+print "\t\t  |\t\t\t\t      |"
+print "\t\t  |\t\t\t\t      |"
+print "\t\t  |\t----------\t     -------  |"
+print "\t\t  |\t|   |    |\t     |     |  |"
+print "\t\t  |\t|---|----|\t     |     |  |"
+print "\t\t  |\t|   |    |\t     |     |  |"
+print "\t\t  |\t----------\t     |     |  |"
+print "\t\t  -------------------------------------"
+
+print "\n\n\t\t\t\t - Simon Jinaphant"
